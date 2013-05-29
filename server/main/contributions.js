f726f6f3f5a0e66f4f00ac8c2b73b792ad4b3fce
Ryan Barry <ryanc.barry@yahoo.com>
2013-05-28 21:23:31 -0700

    Updated contribution model

--- server/main/contributions.js
+++ server/main/contributions.js
@@ -9,9 +9,10 @@ _.extend(Helpers, {
 		var contributionId = Contributions.insert({ 
 			contributorAccountId:contributorAccountId,
 			businessAccountId:businessAccountId,
-			amount:amount,
 			dateReported:new Date(),
-			dateAccepted:null
+			amountReported:amount,
+			dateActivated:null,
+			amountOutstanding:amount
 		});
 
 		// Hook the contribution up to the time accounts.
@@ -27,41 +28,62 @@ _.extend(Helpers, {
 	},
 
 
-	acceptContribution: function(contributionId) {
-		Contributions.update({ _id:contributionId }, { $set:{ dateAccepted:new Date() } });
+	activateContribution: function(contributionId) {
+		Contributions.update({ _id:contributionId }, { $set:{ dateActivated:new Date() } });
 	},
 
-	denyContribution: function(contributionId) {
+	removeContribution: function(contributionId) {
+
+		// d_('Cancelling contribution');
+		// d_(contributionId);
 
 		// Remove association from business account.
 		var contribution = Contributions.findOne({ _id:contributionId });
-		var contributorAccountId = contribution.contributorAccountId;
-		d_(contribution);
+		// d_(contribution);
 
-		var businessAccount = TimeAccounts.findOne({ _id:contribution.businessAccountId });
+		var contributorAccountId = contribution.contributorAccountId;
+
+		var businessAccountId = contribution.businessAccountId;
+		var businessAccount = TimeAccounts.findOne({ _id:businessAccountId });
+
+		// d_('businessAccount');
+		// d_(businessAccount);
+
 		var numContributors = businessAccount.contributors[contributorAccountId].length;
-		d_(businessAccount.contributors[contributorAccountId].length);
 
 		if (numContributors <= 1) {
 			var unset = {};
 			unset['contributors.'+contributorAccountId] = 1;
-			TimeAccounts.update({ _id:contribution.businessAccountId }, { $unset:unset });
+			TimeAccounts.update({ _id:businessAccountId }, { $unset:unset });
 		}
 		else {
 			var pull = {};
 			pull['contributors.'+contributorAccountId] = contributionId;
-			TimeAccounts.update({ _id:contribution.businessAccountId }, { $pull:pull });
+			TimeAccounts.update({ _id:businessAccountId }, { $pull:pull });
 		}
 
 		// Remove association from contributor account.
-		
 
-		// var filteredContributors = _.filter(contributors, function(value) {
-		// 	return (value === contributorAccountId);
-		// });
-		// d_(filteredContributors);
+		// d_('contributorAccount');
+		var contributorAccountId = contribution.contributorAccountId;
+		var contributorAccount = TimeAccounts.findOne({ _id:contribution.contributorAccountId });
+		// d_(contributorAccount);
 
+		var numContributions = contributorAccount.contributions[businessAccountId].length;
 
+		if (numContributions <= 1) {
+			var unset = {};
+			unset['contributions.'+businessAccountId] = 1;
+			TimeAccounts.update({ _id:contributorAccountId }, { $unset:unset });
+		}
+		else {
+			var pull = {};
+			pull['contributions.'+businessAccountId] = contributionId;
+			TimeAccounts.update({ _id:contributorAccountId }, { $pull:pull });
+		}
+
+		// Remove the contribution.
+		Contributions.remove({ _id:contributionId });
 	}
 
 
